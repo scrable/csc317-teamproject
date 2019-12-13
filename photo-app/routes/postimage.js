0b94b8b60d6571fa13ba067a5cc739df0e6e6e69
Corey Russ <cruss.seq11@gmail.com>
2019-12-13 01:38:21 -0800

    add non-working things for posting images

---
+++ photo-app/routes/postimage.js
@@ -0,0 +1,43 @@
+var mysql      = require('mysql');
+var express    = require("express");
+var app = express();
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'csc317db'
+});
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... nn");
+    } else {
+        console.log("Error connecting database ... nn");
+    }
+});
+
+exports.postimage = function (req, res, next){
+    console.log(req.body.description);
+    var imageInfo = {
+        "title":req.body.title,
+        "description":req.body.description,
+    };
+    connection.query('INSERT INTO imageposts SET ?', imageInfo, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred",error);
+            res.send({
+                "code":400,
+                "failed":"error ocurred"
+            })
+        }else{
+            console.log('The solution is: ', results);
+            res.send({
+                "code":200,
+                "success":"user registered sucessfully"
+            });
+        }
+    });
+
+
+    console.log("image sent");
+    res.redirect('../homePage');
+};
