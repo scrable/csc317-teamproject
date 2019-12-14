ea429ccccfdbe4539e04bb01af5ef9fe50c95ac6
Corey Russ <cruss.seq11@gmail.com>
2019-12-14 00:09:12 -0800

    move login to seperate js

--- photo-app/routes/loginroutes.js
+++ photo-app/routes/loginroutes.js
@@ -14,8 +14,6 @@ connection.connect(function(err){
 });
 
 exports.registration = function(req,res){
-    // console.log("req",req.body);
-    var today = new Date();
     var users={
         "username":req.body.username,
         "email":req.body.email,
@@ -39,38 +37,33 @@ exports.registration = function(req,res){
     });
 };
 
-// exports.login = function(req,res){
-//     var username= req.body.username;
-//     var password = req.body.password;
-//     connection.query('SELECT password FROM `csc317db`.`users` WHERE username=?;',[username], function (error, results, fields) {
-//         if (error) {
-//             // console.log("error ocurred",error);
-//             res.send({
-//                 "code":400,
-//                 "failed":"error ocurred"
-//             })
-//         }else{
-//             // console.log('The solution is: ', results);
-//             if(results.length >0){
-//                 if(results[0].password == password){
-//                     //good credentials
-//                     res.cookie('access_token', {token: "123"});
-//                     res.redirect("/")
-//
-//                 }
-//                 else{
-//                     //username != password
-//                     res.redirect("/login")
-//                 }
-//             }
-//             else{
-//                 //user doesn't exist
-//                 res.redirect("/login")
-//             }
-//         }
-//
-//          res.sendFile((__dirname + '/public/login.html'));
-//         // res.render("login");
-//     });
-//
-// };
+exports.login = function(req,res){
+    console.log("login " + req.session.id);
+    if(!req.session.user) {
+        var username = req.body.username;
+        var password = req.body.password;
+        connection.query('SELECT password FROM `csc317db`.`users` WHERE username=?;', [username], function (error, results, fields) {
+            if (error) {
+                res.send({
+                    "code": 400,
+                    "failed": "error ocurred"
+                })
+            } else {
+                if (results.length > 0) {
+                    if (results[0].password == password) {
+                        //good credentials
+                        req.session.user = results[0];
+                        res.redirect("/homePage");
+                    } else {
+                        //username != password
+                        res.redirect("/login")
+                    }
+                } else {
+                    //user doesn't exist
+                    res.redirect("/login")
+                }
+            }
+        });
+    }
+    else res.redirect("/homePage")
+};
